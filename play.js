1b77658c39bb9868a53267632c9ee3c5a31e4c61
Shivam <patelsd96@gmail.com>
2020-01-15 20:37:48 +0000

    made play with two new functions: setupInput and handleUserInput where we use a callback inside the setupInput

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const  connect  = require('./client');
 console.log('Connecting...');
 connect();
+
+const handleUserInput = function(key) {
+  console.log("data event happened")
+  //console.log(key);
+  if (key === '\u0003') {
+      console.log("You exited")
+      process.exit();
+    } 
+  }
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin
+
+}
+
+setupInput();
+
